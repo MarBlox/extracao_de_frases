4cd771379adbdc4bba2b93596e5afcfa5341f28a
Marco Melo <marcomiguelmedeiros2006@gmail.com>
2023-11-05 08:01:42 -0100

    Trabalho Concluido

---
+++ extrecao_de_frases.sh
@@ -0,0 +1,3 @@
+frases=("O dia estava lindo" "o sol brilhava no céu" "as aves cantavam")
+
+grep -o "${frases[0]}" "./texto.txt" && grep -o "${frases[1]}" "./texto.txt" && grep -o "${frases[2]}" "./texto.txt"
