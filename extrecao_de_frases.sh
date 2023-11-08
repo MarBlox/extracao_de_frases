af56e849e964c196365ddbf89198e133153c5073
MarBlox <marcomiguelmedeiros2006@gmail.com>
2023-11-08 12:48:50 -0100

    Trabalho Concluido

--- extrecao_de_frases.sh
+++ extrecao_de_frases.sh
@@ -1,3 +1,5 @@
+#!/bin//bash
+
 frases=("O dia estava lindo" "o sol brilhava no céu" "as aves cantavam")
 
 grep -o "${frases[0]}" "./texto.txt" && grep -o "${frases[1]}" "./texto.txt" && grep -o "${frases[2]}" "./texto.txt"
